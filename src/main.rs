e4040d2de507d939c58d572a9338cdd32bd3ef2e
BookOwl <stanleybookowl@gmail.com>
2017-02-21 14:04:01 -0500

    Add a pattern reversing macro to make op! easier to use

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 
+
 #[derive(Debug)]
 struct Frame {
     num_bindings: usize,
     bindings: Vec<Option<Value>>,
 }
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Value {
     Int(i32),
@@ -35,34 +37,50 @@ enum Error {
     NoMain,
 }
 
-macro_rules! math_op {
-    ($stack: expr => $func: expr) => (
-        {
-            let b = ($stack).pop().ok_or(Error::StackUnderflow)?;
-            let a = ($stack).pop().ok_or(Error::StackUnderflow)?;
-            match (a, b) {
-                (Value::Int(n1), Value::Int(n2)) => {
-                    ($stack).push(Value::Int($func(n1, n2)))
-                },
-                _ => return Err(Error::InvalidTypes),
-            }
-        }
-    )
-}
+// Macros to assist in writing operations.
 
+/// A helper macro for op! that pops as many items from the stack as there are patterns
 macro_rules! pops {
+    // If there is just one patten pop once
     ($stack:ident; $head:pat) => ($stack.pop().ok_or(Error::StackUnderflow)?);
+    // Otherwise pop once for the head pattern and also pop for each of rest.
     ($stack:ident; $head:pat, $($tail:pat),*) => (($stack.pop().ok_or(Error::StackUnderflow)?,
                                             pops!($stack; $($tail),*)));
 }
 
+/// A helper macro for op! that reverses the patterns passed to it so a, b becomes b, a
+macro_rules! reverse_pats {
+    // If there is just one pattern, the reverse is just that pattern
+    ($head:pat) => ($head);
+    // If there is more than one patern the reverse is
+    // the first pattern behind the reverse of the rest of the patterns
+    ($head:pat, $($tail:pat),*) => ((
+        reverse_pats!($($tail),*), $head
+    ));
+}
+
+/// A macro to assist in defining operators.
+/// You pass in the datastack, the patterns for the operator,
+/// and an expression that computes the result of the operator.
+///
+/// Example:
+/// ```
+///Instruction::Add => op!(datastack; Value::Int(a), Value::Int(b) => a + b)
+/// ```
 macro_rules! op {
     ($stack: ident; $( $p:pat ),+ => $exp:expr) => (
         {
+            // Pattern match on same number of pops as there are patterns
             match pops!($stack; $($p),+) {
-                ($($p),+) => {
+                // Reverse the patterns in the match arm so that you can write
+                // the operators like a, b => a + b where b is at the TOS
+                reverse_pats!($($p),+) if true => {
+                    //     ^^^^^^^ This is needed to allow non-exhausitve patterns.
+                    // https://github.com/rust-lang/rust/issues/4112#issuecomment-135410438
                     ($stack).push($exp)
-                }
+                },
+                // Invalid types, so return an Error
+                _ => return Err(Error::InvalidTypes)
             }
         }
     )
@@ -92,13 +110,12 @@ fn run(program: Program) -> Result<(), Error> {
         let instruction = program.instructions.get(ip).ok_or(Error::InstructionOverflow)?;
         match *instruction {
             Instruction::Literal(n) => datastack.push(n),
-            Instruction::Add => math_op!(datastack => |a, b| a + b),
-            Instruction::Sub => math_op!(datastack => |a, b| a - b),
-            Instruction::Mul => math_op!(datastack => |a, b| a * b),
-            Instruction::IntDiv => math_op!(datastack => |a, b| (a / b) as i32),
+            Instruction::Add => op!(datastack; Value::Int(a), Value::Int(b) => Value::Int(a+b)),
+            Instruction::Sub => op!(datastack; Value::Int(a), Value::Int(b) => Value::Int(a-b)),
+            Instruction::Mul => op!(datastack; Value::Int(a), Value::Int(b) => Value::Int(a*b)),
+            Instruction::IntDiv => op!(datastack; Value::Int(a), Value::Int(b) => Value::Int((a/b) as i32)),
             Instruction::Output => {
-                let val = datastack.pop().ok_or(Error::StackUnderflow)?;
-                println!("{:?}", val);
+                println!("{:?}", datastack.pop().ok_or(Error::StackUnderflow)?);
             },
             Instruction::Equal => op!(datastack; a, b => Value::Bool(a == b)),
         };
@@ -115,17 +132,9 @@ fn main() {
     });
     let prog = Program {
             instructions: vec![
-                Instruction::Literal(Value::Int(1)),
-                Instruction::Literal(Value::Int(1)),
-                Instruction::Add,
+                Instruction::Literal(Value::Int(3)),
                 Instruction::Literal(Value::Int(2)),
-                Instruction::Mul,
-                Instruction::Literal(Value::Int(1)),
                 Instruction::Sub,
-                Instruction::Literal(Value::Int(2)),
-                Instruction::IntDiv,
-                Instruction::Literal(Value::Int(1)),
-                Instruction::Equal,
                 Instruction::Output,
             ],
             frame_info: frame_info,
